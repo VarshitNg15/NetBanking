03ac1ab4f1f2d86491e006fe975377e6a1acc2c3
VarshitNg15 <varshithbandary017@gmail.com>
2026-09-17 14:17:43 +0530

    Deleted the bad user service in the separate folder

--- user-service/src/main/java/com/netbanking/user_service/repository/CustomerProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.netbanking.user_service.repository;
-
-import com.netbanking.user_service.entity.CustomerProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerProfileRepository
-        extends JpaRepository<CustomerProfile, String> {
-}
--- user-service/src/main/java/com/netbanking/user_service/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.netbanking.user_service.repository;
-
-import com.netbanking.user_service.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, String> {
-
-    boolean existsByCustomerId(String customerId);
-}
